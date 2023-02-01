d8d97710ecd3b90680b0d9877f9e52007e47c31b
Javier Arturo Rodriguez <codehead@gmail.com>
2023-02-01 12:22:21 +0100

    Added env variable to create named database

--- start-database.sh
+++ start-database.sh
@@ -5,6 +5,7 @@ docker run \
   --env MARIADB_USER=example-user \
   --env MARIADB_PASSWORD=my_cool_secret \
   --env MARIADB_ROOT_PASSWORD=my-secret-pw \
+  --env MARIADB_DATABASE=wordpress \
   -p 3306:3306 \
   -v `pwd`/mariadb-data:/var/lib/mysql \
   mariadb:latest
